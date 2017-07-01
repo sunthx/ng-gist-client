f4ada7118bcd2c9514b3e6a5c389685e60c03c1b
sunthx <sunth0101@126.com>
2017-07-01 18:04:50 +0800

    remove unuse file

--- src/app/service/github-gist.service.ts
+++ src/app/service/github-gist.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { GitHub } from "github-api";
 
 @Injectable()
 export class GithubGistService {
-    constructor() { }
+    constructor() { 
+        
+    }
 }
